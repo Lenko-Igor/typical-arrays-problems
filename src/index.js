6c3e274d7383b3e2154e1e03f9867567c164a706
Igor Lenko <mk2008@mail.ru>
2020-12-24 15:37:18 +0300

    init: start task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 
 exports.min = function min (array) {
-  return 0;
+  return (!array)? 0 : (array.length)? Math.min.apply(null, array) : 0;
 }
 
 exports.max = function max (array) {
-  return 0;
+  return (!array)? 0 : (array.length)? Math.max.apply(null, array) : 0;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  return (!array)? 0 : (array.length)? array.reduce((sum, e) => {return sum + e}, 0) / array.length : 0;
 }
